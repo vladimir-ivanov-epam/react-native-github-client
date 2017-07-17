a83914b86ec4455f59ae2a069a9d7553c06af399
artemkapleev <artem_kapleev@epam.com>
2017-07-17 18:21:47 +0300

    Auth and second screen added

--- index.android.js
+++ index.android.js
@@ -1,42 +1,11 @@
-import React, { Component } from 'react';
-import { View, TextInput, Button, StyleSheet, AppRegistry } from 'react-native';
-import Header from './src/components/header';
-import Inputs from './src/components/inputs';
-import SubmitArea from './src/components/submitArea';
+import { AppRegistry } from 'react-native';
+import App from './src/components/App';
+import UserInfo from './src/components/UserInfo'
+import { StackNavigator } from 'react-navigation';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            logining: false,
-            loginError: false
-        }
-    }
-
-    loginAttempt = () => {
-        this.setState({logining: true, loginError: false});
-        setTimeout(() => {
-            this.setState({logining: false, loginError: true});
-        }, 2000);
-    };
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Header />
-                <Inputs />
-                <SubmitArea logining={this.state.logining} loginError={this.state.loginError} submitHandler={this.loginAttempt} />
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'stretch',
-        backgroundColor: '#ffffff'
-    }
+const AppWithNav = StackNavigator({
+    Login: {screen: App},
+    UserInfo: {screen: UserInfo}
 });
 
-AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('GitHubClient', () => AppWithNav);
--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -4,14 +4,29 @@ import { View, TextInput, StyleSheet } from 'react-native';
 class Inputs extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: this.props.username,
+            password: this.props.password
+        }
     }
 
     render() {
         const { viewStyle, inputStyle } = styles;
         return (
             <View style={viewStyle}>
-                <TextInput style={inputStyle} placeholder="Login" underlineColorAndroid="transparent" />
-                <TextInput style={inputStyle} placeholder="Password" underlineColorAndroid="transparent" secureTextEntry={true} />
+                <TextInput
+                    style={inputStyle}
+                    value={this.state.username}
+                    onChangeText={(val) => { this.props.setUserName(val); this.setState({username: val});}}
+                    placeholder="Login"
+                    underlineColorAndroid="transparent" />
+                <TextInput
+                    style={inputStyle}
+                    value={this.state.password}
+                    onChangeText={(val) => { this.props.setPassword(val); this.setState({password: val});}}
+                    placeholder="Password"
+                    underlineColorAndroid="transparent"
+                    secureTextEntry={true} />
             </View>
         );
     }
